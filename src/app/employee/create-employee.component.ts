3227f4372ea1040317c1b2f696de7666871b3c22
binay dahal <ratedbinaya@gmail.com>
2019-06-25 15:50:24 +0545

    custom validation and more
    added custom validation and more type of validation and array and groups

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormArray, FormControl } from '@angular/forms';
+import { CustomValidators } from '../shared/custom.validators';
 
 @Component({
   selector: 'app-create-employee',
@@ -20,10 +21,22 @@ export class CreateEmployeeComponent implements OnInit {
       'maxlength': 'Full Name must be less than 10 characters.'
     },
     'email': {
-      'required': 'Email is required.'
+      'required': 'Email is required.',
+      'emailDomain': 'Email Domain should be gmail.com.'
+    },
+    'emailGroup': {
+      'emailMismatch': 'Email and confirm email doesnt match',
+
+    },
+
+    'confirmEmail': {
+      'required': 'Confirm Email is required.',
+
     },
     'phone': {
-      'required': 'Phone is required.'
+      'required': 'Phone is required.',
+      //'emailDomain': 'Email Domain should be gmail.com.'
+
     },
     'skillName': {
       'required': 'Skill Name is required.',
@@ -42,6 +55,8 @@ export class CreateEmployeeComponent implements OnInit {
   formErrors = {
     'fullName': '',
     'email': '',
+    'confirmEmail': '',
+    'emailGroup': '',
     'phone': '',
     'skillName': '',
     'experienceInYears': '',
@@ -55,7 +70,11 @@ export class CreateEmployeeComponent implements OnInit {
     this.employeeForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
       contactPreference: ['email'],
-      email: ['', [Validators.required]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, CustomValidators.emailDomain('gmail.com')]],
+        confirmEmail: ['', [Validators.required]]
+      }, { validators: matchEmail }),
+
       phone: [''],
       skills: this.fb.group({
         skillName: ['', [Validators.required]],
@@ -63,15 +82,15 @@ export class CreateEmployeeComponent implements OnInit {
         proficiency: ['', [Validators.required]]
       }),
     });
- 
-   
+
+
     this.employeeForm.get('contactPreference').valueChanges.subscribe((data: string) => {
-  this.onContactPrefernceChange(data);
-});
+      this.onContactPrefernceChange(data);
+    });
 
     this.employeeForm.valueChanges.subscribe((data) => {
       this.logValidationErrors(this.employeeForm);
-    }); 
+    });
   }
 
   onContactPrefernceChange(selectedValue: string) {
@@ -89,39 +108,65 @@ export class CreateEmployeeComponent implements OnInit {
     Object.keys(group.controls).forEach((key: string) => {
       // Get the control. The control can be a nested form group
       const abstractControl = group.get(key);
-      // If the control is nested form group, recursively call
-      // this same method
+
+
+      // Clear the existing validation errors
+      this.formErrors[key] = '';
+
+      if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+        // Get all the validation messages of the form control
+        // that has failed the validation
+        const messages = this.validationMessages[key];
+        // Find which validation has failed. For example required,
+        // minlength or maxlength. Store that error message in the
+        // formErrors object. The UI will bind to this object to
+        // display the validation errors
+
+        for (const errorKey in abstractControl.errors) {
+          if (errorKey) {
+            this.formErrors[key] += messages[errorKey] + ' ';
+          }
+        }
+      }
+
       if (abstractControl instanceof FormGroup) {
         this.logValidationErrors(abstractControl);
         // If the control is a FormControl
-      } else {
-        // Clear the existing validation errors
-        this.formErrors[key] = '';
-
-        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
-          // Get all the validation messages of the form control
-          // that has failed the validation
-          const messages = this.validationMessages[key];
-          // Find which validation has failed. For example required,
-          // minlength or maxlength. Store that error message in the
-          // formErrors object. The UI will bind to this object to
-          // display the validation errors
-
-          for (const errorKey in abstractControl.errors) {
-            if (errorKey) {
-              this.formErrors[key] += messages[errorKey] + ' ';
-            }
-          }
-        }
       }
     });
   }
   onLoadDataClick(): void {
-    // this.logValidationErrors(this.employeeForm);
-    // console.log(this.formErrors);
+
+    const formArray1 = this.fb.array([
+      new FormControl('John', Validators.required),
+      new FormControl('IT', Validators.required),
+      new FormControl('male', Validators.required),
+    ]);
+
+    const formGroup = this.fb.group([
+      new FormControl('John', Validators.required),
+      new FormControl('IT', Validators.required),
+      new FormControl('male', Validators.required),
+    ]);
+
+    console.log(formArray1);
+    console.log(formGroup);
   }
 
   onSubmit(): void {
     console.log(this.employeeForm.value);
   }
 }
+
+function matchEmail(group: AbstractControl): { [key: string]: any } | null {
+  const emailControl = group.get('email');
+  const confirmEmailControl = group.get('confirmEmail');
+  if (emailControl.value === confirmEmailControl.value || confirmEmailControl.pristine) {
+    return null;
+  }
+  else {
+    return { 'emailMismatch': true };
+  }
+
+}
+
