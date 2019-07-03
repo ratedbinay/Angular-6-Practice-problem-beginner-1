8b444bfddc4f47dca4e700dd5efda53b0361bff4
binay dahal <ratedbinaya@gmail.com>
2019-07-03 11:39:32 +0545

    shared and freature modules 34 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CreateEmployeeComponent } from './employee/create-employee.component';
-import { ListEmployeesComponent } from './employee/list-employee.component';
+import { EmployeeModule } from './employee/employee.module';
+
 import {EmployeeService} from './employee/employee.service';
+
+import { AppComponent } from './app.component';
+
 import { HomeComponent } from './home.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateEmployeeComponent,
-    ListEmployeesComponent,
     HomeComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    EmployeeModule,
+    AppRoutingModule,        
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
