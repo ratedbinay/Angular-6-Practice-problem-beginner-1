a83fada4152bcbd90747fc030fbab05c989deee8
binay dahal <ratedbinaya@gmail.com>
2019-07-03 17:05:42 +0545

    completed
    added custom preloading angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { CustomPreloadingService } from './custom-preloading.service';
 
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'employees', loadChildren: './employee/employee.module#EmployeeModule' },
+  { path: 'employees', data:{preload:true}, loadChildren: './employee/employee.module#EmployeeModule' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })],
+  imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: CustomPreloadingService })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
